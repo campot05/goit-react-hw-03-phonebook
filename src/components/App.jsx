f8b8ca2bdd39782874c460565ee6d52c974df7c0
Danylo Trutniev <117174027+campot05@users.noreply.github.com>
2023-02-26 21:47:03 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+
+    filter: '',
+  };
+
+  submitFormData = ({ name, number }) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    for (const contact of this.state.contacts) {
+      console.log(contact);
+      if (contact.name === name) {
+        return Notify.failure(`${name} is already in contacts.`);
+      }
+    }
+
+    this.setState(({ contacts }) => ({ contacts: [newContact, ...contacts] }));
+  };
+
+  filter = e => {
+    console.log(e.currentTarget.value);
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  visibleContact = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  deleteContact = id => {
+    this.setState(prev => {
+      return {
+        contacts: prev.contacts.filter(el => el.id !== id),
+      };
+    });
+  };
+  render() {
+    return (
+      <div className="thumb">
+        <h1>Phonebook</h1>
+        <ContactForm
+          onSubmit={this.submitFormData}
+          data={this.state.contacts}
+        />
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} filter={this.filter} />
+        <ContactList
+          contacts={this.visibleContact()}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,66 @@
+import { Component } from 'react';
+import css from './ContactForm.module.css';
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleNameChange = e => {
+    this.setState({ name: e.currentTarget.value });
+  };
+
+  handleNumberChange = e => {
+    this.setState({ number: e.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className={css.contactForm}>
+        <label>
+          Name <br />
+          <input
+            className={css.input}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={this.state.name}
+            onChange={this.handleNameChange}
+          />
+        </label>
+        <br />
+        <label>
+          Number <br />
+          <input
+            className={css.input}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={this.state.number}
+            onChange={this.handleNumberChange}
+          />
+        </label>
+        <br />
+        <button className={css.btn} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
